c504838e9e1651ab87a62ebe7a3dec9d25f6b0c5
Пётр Фирсов <peterfirsoff@gmail.com>
2023-08-10 22:22:57 +0300

    Третья версия домашнего задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,10 +3,9 @@ function solveEquation(a, b, c) {
   let arr = [];
   let d = (b**2-4*a*c);
 
-  if (d < 0) {
-  arr = [];
-  } else if (d === 0) {
+  if (d === 0) {
   let onlyRoot = (-b/(2*a));
+  arr.push(onlyRoot);
   } else if (d > 0) {
   let firstRoot = (-b + Math.sqrt(d) )/(2*a);
   let secondRoot = (-b - Math.sqrt(d) )/(2*a);
@@ -14,23 +13,21 @@ function solveEquation(a, b, c) {
   arr.push(secondRoot);
   } 
 
-  console.log(arr);
+  return(arr);
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
-  percent/1200
-  let body = amount - contribution;
-  let monthlyPercent = percent/12;
+  let body = (amount - contribution);
+  let monthlyPercent = percent/1200;
 
-  let total = body * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**countMonths) - 1)));
+  let creditPercent = body * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**countMonths) - 1)));
+
+  let total = creditPercent * countMonths;
 
   total.toFixed(2);
 
-  console.log(total);
+  +total;
+
+  return(total);
 }
-
-solveEquation(1, 6, 1);
-
-
-calculateTotalMortgage(10, 0, 50000, 12);
