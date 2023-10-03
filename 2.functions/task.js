c539d21ab940546488ab9252c092a158c2e4775e
Пётр Фирсов <peterfirsoff@gmail.com>
2023-10-03 13:59:32 +0300

    Initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,24 @@
-function getArrayParams(...arr) {
+function getArrayParams(...args) {
+  let min = Infinity;
+  let max = -Infinity;
+
+  for (let i = 0; i < X.length; i++) {
+    if X[i] > max,
+    let max = X[i];
+
+    if X[i] < max,
+    let min = X[i];
+
+    let sum = ++i;
+  }
+
+  let avg = sum/X.length;
+  toFixed.avg(2);
+  ++avg;
 
   return { min: min, max: max, avg: avg };
 }
+
 
 function summElementsWorker(...arr) {
 
