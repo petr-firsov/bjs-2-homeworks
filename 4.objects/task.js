5becf65c8ae00f85cba53c3fd7cc76a1d5851755
Пётр Фирсов <peterfirsoff@gmail.com>
2023-12-08 00:14:57 +0300

    Первая партия

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,53 @@
+// Задание 1
+
 function Student(name, gender, age) {
-  
+   this.name = name;
+   this.gender = gender;
+   this.age = age; 
 }
+
+Student.prototype.marks = [];
+
+new Student('Oleg', 'male', 29); 
+
+new Student('Maria', 'female', 22);
+
+new Student('Alevtina', 'female', 19)
+
+
+
+// Задание 2
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+
+// Задание 3
+
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (Student.hasOwnProperty('marks') === true) {
+    this.marks = args;
+    }
 }
 
-Student.prototype.getAverage = function () {
-  
+
+//  Задание 4
+
+Student.prototype.getAverage = function (...marksToAdd) {
+    if (Student.hasOwnProperty('marks') === true) {
+        let sum = 0;
+        let length = marksToAdd.length;
+        marksToAdd.forEach(function(number) {
+        sum += number;
+        return sum / length; 
+    }) } else return 0;
 }
+
+
+
+// Задание 5
 
 Student.prototype.exclude = function (reason) {
-  
+    
 }
