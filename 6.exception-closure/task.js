74e0cb27964591da49f8a85408f5afb19c1af763
Пётр Фирсов <peterfirsoff@gmail.com>
2024-01-20 11:29:43 +0300

    Первая попытка

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,64 @@
-﻿
+﻿// Задача 1
+
+
+parseCount = function(value) {
+    try {
+        Number.parseFloat(value) === NaN;
+    } catch(error) {
+        console.log(error);
+        throw('Невалидное значение');
+    } finally {
+        return Number.parseFloat(value);
+    }
+}
+
+
+validateCount = function(value) {
+    try {
+        parseCount(value);
+    } catch(error) {
+        throw(error);
+    } finally {
+        return parseCount(value);
+    }
+}
+
+
+
+
+// Задача 2
+
+class Triangle {
+    constructor(a, b, c) {
+        try {
+            if ((a + b < c) || (a + c <b) || (b + c < a) === true); 
+        } catch (error) {
+            console.log(error);
+            console.log('Треугольник с такими сторонами не существует');
+        }
+    }
+
+    get perimeter() {
+        return a + b + c;
+    }
+
+    get area() {
+        let p = 1/2 * (a + b + c);
+        let s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return s;
+    }
+}
+
+getTriangle = function() {
+    try { 
+        new Triangle;
+        if (Triangle.constructor.catch === error);
+    } catch {
+        Triangle.perimeter();
+        Triangle.area();
+        throw('Ошибка! Треугольник не существует'); 
+    }
+}
+
+
+   
