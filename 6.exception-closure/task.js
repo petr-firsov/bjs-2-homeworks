7aa3f00b2bc9176a320b5d279e4ac55755b16f0d
Пётр Фирсов <peterfirsoff@gmail.com>
2024-01-27 22:34:57 +0300

    Финальная версия

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -25,28 +25,19 @@ class Triangle {
     constructor(a, b, c) {
         if ((a + b < c) || (a + c < b) || (b + c < a) === true) {
             throw new Error('Треугольник с такими сторонами не существует');
-        } else {
+        } 
             this.a = a;
             this.b = b;
             this.c = c;
         }
-    }
 
     get perimeter() {
-        if (this.constructor === Error) {
-            return 'Ошибка! Треугольник не существует';
-        } else {
             return this.a + this.b + this.c;
-        }
     }
 
     get area() {
-        if (this.constructor === Error) {
-            return 'Ошибка! Треугольник не существует';
-        } else {
             let p = this.perimeter / 2;
             return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
-        }
     }
 }
 
@@ -54,7 +45,17 @@ function getTriangle(a, b, c) {
     try {
         return new Triangle(a, b, c);
     } catch(error) {
-        return Triangle.perimeter, Triangle.area;
+        class errorClass {
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            }
+
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+        let errorObject = new errorClass();
+        return errorObject;
     }
 }
 
